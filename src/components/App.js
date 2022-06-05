6429f6631d6e72e5d9ff9b1d4b86578915f19758
angelayke <angebee2605@aol.com>
2022-06-05 16:57:19 +0200

    modification app et components

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import IconStar from '../images/icon-star.svg';
+
+
+import Container from './Container';
+import Button from './Button';
+import Text from './Text';
+import Image from './Image';
+
+function App() {
+  return (
+    <div>
+    <Container>
+      <Button>
+       <Image src={IconStar} alt="Icon Star"/>
+      </Button>
+      
+      <Text as="h2">
+        How did we do?
+      </Text>
+      <Text as="p">
+        Please let us know how we did with your support request. All feedback is appreciated 
+        to help us improve our offering!
+      </Text>
+      <div>
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+        <Button>4</Button>
+        <Button>5</Button>
+      </div>
+      <Button>
+        SUBMIT
+      </Button>
+    </Container>
+  </div>
+  );
+}
+
+export default App;
