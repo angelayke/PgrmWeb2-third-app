60b29ec65f5bc894d9531cce9b87df17bb088751
angelayke <angebee2605@aol.com>
2022-06-06 07:25:52 +0200

    dernier commit push

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -7,14 +7,17 @@ const Button = ({ as = "button", children}) => {
     default:
          return <button className={as}>{children}</button>;
   }
-  const handleButtonClick = (event) => {
-      console.log(event.target);
-  }
 
-    return (
-    <button className='button' onClick={handleButtonClick}>{children}</button>
-  )
+  const handleButtonClick = (event) => {
+    console.log(event.target);
 }
+
+  return (
+  <button className='button' onClick={handleButtonClick}>{children}</button>
+)
+}
+  
+
 
 
 export default Button
